b83292ada874c56f7983f34b385585ef90606694
SoniaSul <sonia.sultani1365@gmail.com>
2025-04-02 10:12:32 +0200

    World Map pagina toegevoegd en play-knop gefixt

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-world-map',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './world-map.component.html',
   styleUrl: './world-map.component.css'
 })
