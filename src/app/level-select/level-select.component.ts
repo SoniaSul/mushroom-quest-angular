c8f8ac087a5a12104f270b9c9377b044f285d573
SoniaSul <sonia.sultani1365@gmail.com>
2025-04-09 10:10:05 +0200

    Levels pagina is gemaakt

--- src/app/level-select/level-select.component.ts
+++ src/app/level-select/level-select.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-level-select',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './level-select.component.html',
   styleUrl: './level-select.component.css'
 })
 export class LevelSelectComponent {
-
+  levels = [
+    { number: 1, locked: false },
+    { number: 2, locked: false },
+    { number: 3, locked: false },
+    { number: 4, locked: false },
+    { number: 5, locked: false },
+    { number: 6, locked: false },
+    { number: 7, locked: false },
+    { number: 8, locked: false },
+    { number: 9, locked: false },
+    { number: 10, locked: false },
+    { number: 11, locked: false },
+    { number: 12, locked: false },
+    { number: 13, locked: true },
+    { number: 14, locked: true },
+    { number: 15, locked: true },
+  ];
 }
