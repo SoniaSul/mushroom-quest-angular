8d436603f16682c996d66867b45256d5479c0aee
SoniaSul <sonia.sultani1365@gmail.com>
2025-04-16 20:50:49 +0200

    Help  pagina is gemaakt

--- src/app/help-page/help-page.component.ts
+++ src/app/help-page/help-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-help-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './help-page.component.html',
   styleUrl: './help-page.component.css'
 })
