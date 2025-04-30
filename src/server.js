eff60df448bc5f06b056884d8de869c41e898640
SoniaSul <sonia.sultani1365@gmail.com>
2025-04-30 14:14:07 +0200

    add server.js

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/your-project-name')));
+app.use(express.static(path.join(__dirname, 'dist/mushroom-quest')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/your-project-name/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/mushroom-quest/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
