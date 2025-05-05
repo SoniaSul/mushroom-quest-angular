2c3fd4c4cacb38d994e7ccf81a844c3db276587e
SoniaSul <sonia.sultani1365@gmail.com>
2025-05-05 12:34:46 +0200

    updatr

--- src/app/level-select/level-select.component.ts
+++ src/app/level-select/level-select.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-level-select',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './level-select.component.html',
   styleUrl: './level-select.component.css'
 })
